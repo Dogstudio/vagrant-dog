37e2dba0e202c6e6f17c5a1fea63a3256ba40783
Thierry Lagasse <epagneul@dogstudio.be>
2015-07-24 15:08:04 +0200

    Test install

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -5,6 +5,31 @@
 #   Author: Thierry 'Epagneul' Lagasse <epagneul@dogstudio.be>
 #   Since: Jul 2015
 #
+# =============================================================================
+
+SEP="$(printf '%0.1s' "-"{1..80})"$'\n'
+
+# -----------------------------------------------------------------------------
+
+function echo_success { echo -ne "$1\033[70G\033[0;39m[   \033[1;32mOK\033[0;39m    ]\n"; }
+function echo_failure { echo -ne "$1\033[70G\033[0;39m[ \033[1;31mFAILED\033[0;39m  ]\n"; }
+
+# -----------------------------------------------------------------------------
+
+function confirm {
+    echo -en "\a" ; read -p "$1" BOOL_CONFIRM
+    if [[ $BOOL_CONFIRM =~ [YyOo] ]]; then true ; else false ; fi
+}
+
+function askcontinue {
+    READVAR="$2"
+    if [ -n "$READVAR" ] || ! read -p "$1" READVAR || [ -n "$READVAR" ]; then
+        echo "$READVAR"
+    else
+        false
+    fi
+}
+
 # -----------------------------------------------------------------------------
 
 # Get current script
@@ -12,12 +37,14 @@ SCRIPT_URL="git@gitlab.dogstudio.be:devtools/vagrantdog.git"
 SCRIPT_BRANCH="develop"
 SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd -P)
 
-
 # Install from remote 
 if [[ $(basename $0) != "install.sh" ]]; then
     echo -e "\tDownloading"
     git archive --remote $SCRIPT_URL $SCRIPT_BRANCH | tar -x -C ./ && bash install.sh
 
+# Install localy
+else
+    echo "URL: ${SCRIPT_URL}"
+    echo "PATH: ${SCRIPT_PATH}"
 
-echo "URL: ${SCRIPT_URL}"
-echo "PATH: ${SCRIPT_PATH}"
+fi
